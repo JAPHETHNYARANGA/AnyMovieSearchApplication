3f5adbcf7f1ba7e6e8afbcfb531e55dd8eda1660
JAPHETHNYARANGA <nyaranga4@gmail.com>
2022-12-19 10:32:49 +0300

    created retrofit service class and dependency injection object

---
+++ app/src/main/java/com/example/tvmazemvvm/data/network/MoviesService.kt
@@ -0,0 +1,11 @@
+package com.example.tvmazemvvm.data.network
+
+import com.example.tvmazemvvm.domain.models.Model
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MoviesService {
+    @GET("shows")
+    suspend fun getAllMovies(@Query("q")movieName:String) : Response<Model>
+}
---
+++ app/src/main/java/com/example/tvmazemvvm/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.example.tvmazemvvm.di
+
+object AppModule {
+}
